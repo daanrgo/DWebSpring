d290c8a13a3c766f275405a68034809f76debe16
Isaac Janica <95540706+SakiJan24@users.noreply.github.com>
2025-03-02 06:55:11 -0500

    updated the tarjetas_comidas file with thymeleaf

--- portico/src/main/java/com/example/portico/controlador/ComidaController.java
+++ portico/src/main/java/com/example/portico/controlador/ComidaController.java
@@ -21,8 +21,16 @@ public class ComidaController {
     @GetMapping("/info")    
     public String mostrarInfoComida(Model model){
 
+
         model.addAttribute("comidas",comidaService.SearchAll());
         return "tabla_comida";
+   }
+
+   @GetMapping("/hamburguesas")
+   public String mostraHamburguesas(Model model) {
+
+        model.addAttribute("comidas", comidaService.SearchAll());
+        return "tarjetas_comidas";
    }
 
    
