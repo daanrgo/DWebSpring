97335748068fed79173b8578ebf72a54254ddce9
Isaac Janica <95540706+SakiJan24@users.noreply.github.com>
2025-03-02 06:58:06 -0500

    had to correct something in the comidaRepository

--- portico/src/main/java/com/example/portico/repositorio/ComidaRepository.java
+++ portico/src/main/java/com/example/portico/repositorio/ComidaRepository.java
@@ -19,7 +19,7 @@ public class ComidaRepository {
         data.put(1, new Comida(1, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "/portico_spring/portico/src/main/resources/static/images/burger4.png"));
         data.put(2, new Comida(2, "Hamburguesa \"El Noble\"", 30000, "\"Una forma elegante de comer sabroso\"", "/portico_spring/portico/src/main/resources/static/images/hamburgesa.png"));
         data.put(3, new Comida(3, "Hamburguesa \"El Príncipe\"", 35000, "\"No necesitas trono para comerla\"", "/portico_spring/portico/src/main/resources/static/images/burger6.png"));
-        data.put(4, new Comida(4, "Hamburguesa Con Queso", 30000, "Es una hamburguesa con triple carne, sencillo", "https://imagenes.eltiempo.com/files/image_1200_600/uploads/2024/05/03/66351b3e54969.jpeg"));
+        data.put(5, new Comida(4, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "/portico_spring/portico/src/main/resources/static/images/burger4.png"));
         data.put(5, new Comida(5, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "/portico_spring/portico/src/main/resources/static/images/burger4.png"));
         data.put(6, new Comida(6, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "/portico_spring/portico/src/main/resources/static/images/burger4.png"));
         data.put(7, new Comida(7, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "/portico_spring/portico/src/main/resources/static/images/burger4.png"));
