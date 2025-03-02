cdd8795b8d368c19b51756db71c01244f86a9026
Spdrd <juandiegoreyesrodriguez@gmail.com>
2025-03-02 17:09:49 -0500

    Error fotos solucionado

--- portico/src/main/java/com/example/portico/repositorio/ComidaRepository.java
+++ portico/src/main/java/com/example/portico/repositorio/ComidaRepository.java
@@ -16,15 +16,15 @@ public class ComidaRepository {
 
     public ComidaRepository() {
 
-        data.put(1, new Comida(1, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "burger4.png"));
-        data.put(2, new Comida(2, "Hamburguesa \"El Noble\"", 30000, "\"Una forma elegante de comer sabroso\"", "hamburgesa.png"));
-        data.put(3, new Comida(3, "Hamburguesa \"El Príncipe\"", 35000, "\"No necesitas trono para comerla\"", "burger6.png"));
-        data.put(5, new Comida(4, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "burger4.png"));
-        data.put(5, new Comida(5, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "burger4.png"));
-        data.put(6, new Comida(6, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "burger4.png"));
-        data.put(7, new Comida(7, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "burger4.png"));
-        data.put(8, new Comida(8, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "burger4.png"));
-        data.put(9, new Comida(9, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "burger4.png"));
+        data.put(1, new Comida(1, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "/images/burger4.png"));
+        data.put(2, new Comida(2, "Hamburguesa \"El Noble\"", 30000, "\"Una forma elegante de comer sabroso\"", "/images/hamburgesa.png"));
+        data.put(3, new Comida(3, "Hamburguesa \"El Príncipe\"", 35000, "\"No necesitas trono para comerla\"", "/images/burger6.png"));
+        data.put(5, new Comida(4, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "/images/burger4.png"));
+        data.put(5, new Comida(5, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "/images/burger4.png"));
+        data.put(6, new Comida(6, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "/images/burger4.png"));
+        data.put(7, new Comida(7, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "/images/burger4.png"));
+        data.put(8, new Comida(8, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "/images/burger4.png"));
+        data.put(9, new Comida(9, "Hamburguesa \"El Portico\"", 25000, "\"Con esta te sientes en casa\"", "/images/burger4.png"));
 
 
     }
