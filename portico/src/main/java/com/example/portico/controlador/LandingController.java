e146fa144b7251d574e3b205c8ff569f1b2d532b
danieleon08 <dflp2005@gmail.com>
2025-03-08 16:50:15 -0500

    Arreglo diseños y navegacion

--- portico/src/main/java/com/example/portico/controlador/LandingController.java
+++ portico/src/main/java/com/example/portico/controlador/LandingController.java
@@ -1,6 +1,7 @@
 package com.example.portico.controlador;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,5 +13,10 @@ public class LandingController {
     @GetMapping("")
     public String landing(){
         return "landing";
+    }
+
+     @GetMapping("/somos")
+    public String mostrarQuienesSomos() {
+        return "somos";
     }
 }
